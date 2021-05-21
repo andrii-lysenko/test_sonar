d1b10df01d25333c005063b766951745d4b6c919
Andrii Lysenko <lysenkoa.work@gmail.com>
2021-05-21 15:16:45 +0300

    more bad code

--- src/index.js
+++ src/index.js
@@ -20,6 +20,28 @@ const duplicateFunction = (value) => {
     console.log(value);
 }
 
+const duplicateFunction2 = (value) => {
+    console.log(value);
+    value = value;
+    value = 2;
+    do {
+        if (value === 3) break;
+        console.log(value);
+        break;
+    } while (true);
+    
+    for (let i = 2; i < 10; i++) {
+        const value = 3;
+        console.log(value);
+    }
+    try {
+        console.log(value);
+    } catch (error) {
+        alert(error.stack);
+    }
+    
+}
+
 const badCondition = (value) => {
     if (value == 2) {
         return true;
@@ -31,6 +53,14 @@ const infiniteLoop = (value) => {
     infiniteLoop(value);
 }
 
+const messWindow = (val) => {
+    window.top.location.href = val.href;
+}
+
+const messWindow = (val) => {
+    window.top.location.href = val.href;
+}
+
 const hello = () => {
     console.log('hello');
     return true;
@@ -38,5 +68,8 @@ const hello = () => {
 
 module.exports = {
     hello,
-    badCondition
+    badCondition,
+    overrideParam,
+    duplicateFunction,
+    messWindow
 }
